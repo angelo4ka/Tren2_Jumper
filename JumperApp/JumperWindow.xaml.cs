219286c8ca50f70ae697c6f00955b64d6dfa6ddf
angelo4ka <karina3125@yandex.ru>
2022-12-01 00:44:15 +0300

    Add project files.

---
+++ JumperApp/JumperWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using JumperApp.AppDataFile;
+using JumperApp.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JumperApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FrameObj.frameMain = FrmMain;
+
+            FrmMain.Navigate(new PageSubsystem());
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (FrameObj.frameMain.CanGoBack)
+            {
+                FrameObj.frameMain.GoBack();
+            }
+        }
+
+        private void BtnInput_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ JumperApp/Pages/PageSubsystem.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JumperApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for PageSubsystem.xaml
+    /// </summary>
+    public partial class PageSubsystem : Page
+    {
+        public PageSubsystem()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAgents_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnManufacture_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddAgent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnWarehouseMaterials_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEmployees_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
